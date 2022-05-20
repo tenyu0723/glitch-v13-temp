4ecc8e5f20a04458ed29e7a158c4f29c026f53f6
tenyu0723 <90138515+tenyu0723@users.noreply.github.com>
2022-05-21 00:13:44 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+const http = require('http')
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { Pagination } = require("discordjs-button-embed-pagination");
+const moment = require('moment');
+const Keyv = require('keyv');
+const client = new Client({
+  partials: ["CHANNEL"],
+  intents: new Intents(32767),
+  restTimeOffset: -1000
+});
+const newbutton = (buttondata) => {
+  return {
+    components: buttondata.map((data) => {
+      return {
+        custom_id: data.id,
+        label: data.label,
+        style: data.style || 1,
+        url: data.url,
+        emoji: data.emoji,
+        disabled: data.disabled,
+        type: 2,
+      };
+    }),
+    type: 1,
+  };
+};
+const prefix = ""
+const cmd_list = []
+const json = require("./command.json")
+const clean = async (text) => {
+if (text && text.constructor.name == "Promise")
+  text = await text;
+if (typeof text !== "string")
+  text = require("util").inspect(text, { depth: 1 });
+  return text;
+}
+process.env.TZ = 'Asia/Tokyo'
+
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
+    response.end(`${client.user.tag} is ready!\n導入サーバー:${client.guilds.cache.size}\nユーザー:${client.users.cache.size}`)
+  })
+  .listen(3000)
+
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+  console.error('tokenが設定されていません！')
+  process.exit(0)
+}
+
+client.on('ready', async () => {
+  const kamui = client.users.cache.get(ID)
+    client.user.setActivity(`NONE | prefix=${prefix} | 製作者:${kamui.tag} | ${client.guilds.cache.size}servers | ${client.users.cache.size}members`, {
+      type: 'PLAYING'
+    });
+    client.user.setStatus("idle");
+  console.log(`${client.user.tag} is ready!`);
+  const embed = new MessageEmbed()
+  .setTitle("起動しました！")
+  .setDescription(">>> ```diff\n+ Hello World!　　　　　``````diff\n+ 導入サーバー数:" + client.guilds.cache.size + "\n+ ユーザー数:" + client.users.cache.size +  "\n+ メンテナンス:" + await maintenance.get("maintenance") + "```" + moment().format("YYYY-MM-DD HH:mm:ss"))
+  .setThumbnail(client.user.displayAvatarURL())
+  .setColor("RANDOM")
+  client.channels.cache.get(ID).send({ embeds:[embed] })
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN)
